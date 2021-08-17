be60b5109be5b3db43607e770dd11fe974d43e1c
RenukaThakre <88188595+RenukaThakre@users.noreply.github.com>
2021-08-17 15:56:03 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import Circle from "./components/Circle";
-var c1 = new Circle("Blue");
-c1.calculateArea();
 ReactDOM.render(<App />, document.getElementById("root"));
